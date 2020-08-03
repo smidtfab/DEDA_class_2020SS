2f8ce5a059ec70e491d7cad3e66ba0c91049b67e
Fabian Schmidt <fabis@outlook.com>
2020-08-04 00:10:28 +0200

    changed structure for tweets

--- DEDA_2020SS_Crypto_Volatility/CV01_Scraping_Twitter/scrape_for_day.sh
+++ DEDA_2020SS_Crypto_Volatility/CV01_Scraping_Twitter/scrape_for_day.sh
@@ -1,6 +1,6 @@
 # slightly malformed input data
-input_start=2020-4-30
-input_end=2020-5-6
+input_start=2020-1-1
+input_end=2020-1-2
 
 # After this, startdate and enddate will be valid ISO 8601 dates,
 # or the script will have aborted when it encountered unparseable data
@@ -16,8 +16,8 @@ while [ "$d" != "$enddate" ]; do
   echo "Scraping tweets on" $d
   bd=$d # set begin date
   d=$(date -I -d "$d + 1 day") # offset day by one to scrape for $d
-  fn="data/by_day/tweets_btc_${bd}.csv" # file name
-  limit=4000 # tweets per day
+  fn="../data/Tweets/by_day/tweets_btc_${bd}.csv" # file name
+  limit=1000 # tweets per day
   lang="english"
   twitterscraper "Bitcoin AND BTC" -l $limit -bd $bd -ed $d --lang $lang -c -o $fn
 done
